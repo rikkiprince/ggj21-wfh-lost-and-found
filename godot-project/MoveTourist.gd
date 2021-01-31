f55846fe8d9580e39d5035032c9e88b2ad52cd0c
Rikki Prince <rfp@ecs.soton.ac.uk>
2021-01-31 01:29:47 -0700

    Tourist walks around randomly

--- godot-project/MoveTourist.gd
+++ godot-project/MoveTourist.gd
@@ -5,23 +5,44 @@ extends KinematicBody2D
 export (int) var speed = 30
 var velocity = Vector2()
 onready var tilemap = get_parent().get_node("TileMap")
+var position_on_map = Vector2()
+var direction = Vector2()
 
-func get_input():
-	velocity = Vector2()
-	velocity.x = -1
-	velocity = velocity.normalized() * speed
+const N = Vector2(0,-1)
+const S = Vector2(0,1)
+const E = Vector2(1,0)
+const W = Vector2(-1,0)
+var directions = [N, S, E, W]
+
+const STOOD_STILL = Vector2(0,0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position_on_map = tilemap.position_on_map(self)
+	direction = pick_random_direction()
+	randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+
+func pick_random_direction():
+	return directions[randi() % directions.size()]
+
+
 func _physics_process(delta):
-	get_input()
+	var new_position_on_map = tilemap.position_on_map(self)
+	if(new_position_on_map != position_on_map):
+		if(tilemap.is_on_intersection(self)):
+			direction = pick_random_direction()
+			# TODO: Check direction has road
+			# TODO: If possible, don't U-turn
+			print("Changed direction: "+str(direction))
+	position_on_map = new_position_on_map
+	
+	velocity = direction.normalized() * speed
 	velocity = move_and_slide(velocity)
-	tilemap.is_on_intersection(self)
+	
--- godot-project/TileMap.gd
+++ godot-project/TileMap.gd
@@ -8,10 +8,7 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("0,0: "+str(get_cell(0,0)))
-	print("10,0: "+str(get_cell(10,0)))
-	print("0,10: "+str(get_cell(0,10)))
-	print("10,10: "+str(get_cell(10,10)))
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,4 +18,12 @@ func _ready():
 func is_on_intersection(tourist):
 	var map_position_of_tourist = world_to_map(tourist.position)
 	#print(map_position_of_tourist)
-	var cell_on = get_cellv(map_position_of_tourist)
+	var tile_on = get_cellv(map_position_of_tourist)
+	var tile_name = tile_set.tile_get_name(tile_on)
+	if(tile_name == "Intersection"):
+		return true
+	return false
+
+
+func position_on_map(tourist):
+	return world_to_map(tourist.position)

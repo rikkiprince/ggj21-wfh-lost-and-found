fbb105a47440e47b18ccf0db92f8e62498c11e7b
Rikki Prince <rfp@ecs.soton.ac.uk>
2021-01-31 18:20:41 -0700

    Handle a simple list of directions

--- godot-project/MoveTourist.gd
+++ godot-project/MoveTourist.gd
@@ -12,15 +12,16 @@ const N = Vector2(0,-1)
 const S = Vector2(0,1)
 const E = Vector2(1,0)
 const W = Vector2(-1,0)
-var directions = [N, S, E, W]
+var compass_directions = [N, S, E, W]
 
 const STOOD_STILL = Vector2(0,0)
 
+var list_of_directions = ["south","left","straight","right","west"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position_on_map = tilemap.position_on_map(self)
-	direction = pick_random_direction()
+	direction = E
 	randomize()
 
 
@@ -30,15 +31,37 @@ func _ready():
 
 
 func pick_random_direction():
-	return directions[randi() % directions.size()]
+	return compass_directions[randi() % compass_directions.size()]
+	
+func relative_to_compass(current_direction, relative_turn):
+	match relative_turn.to_lower():
+		"left":
+			return current_direction.rotated(deg2rad(-90))
+		"right":
+			return current_direction.rotated(deg2rad(90))
+		_:
+			return current_direction
 
+func get_next_direction():
+	var direction_text = list_of_directions.pop_front()
+	if(["north","east","south","west"].has(direction_text)):
+		print("Got compass direction: "+direction_text)
+		var new_compass_direction = direction_text[0].to_upper()
+		if(new_compass_direction in self):
+			return self.get(new_compass_direction)
+		else:
+			print("ERROR: Could not find compass direction "+str(new_compass_direction))
+	elif(["left","right","straight"].has(direction_text)):
+		return relative_to_compass(direction, direction_text)
+	
+	return STOOD_STILL # pick_random_direction()
 
 func _physics_process(delta):
 	var new_position_on_map = tilemap.position_on_map(self)
 	if(new_position_on_map != position_on_map):
 		# TODO: Am I next to destination?
 		if(tilemap.is_on_intersection(self)):
-			direction = pick_random_direction()
+			direction = get_next_direction()
 			# TODO: Check direction has road
 			# TODO: If possible, don't U-turn
 			print("Changed direction: "+str(direction))

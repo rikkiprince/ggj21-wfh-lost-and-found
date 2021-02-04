0930f9a578ae1a28b5e152346b10ec50812add75
Rikki Prince <rfp@ecs.soton.ac.uk>
2021-02-04 00:50:40 -0700

    Add reference to tourist to ConversationPanel

--- godot-project/ConversationPanel.gd
+++ godot-project/ConversationPanel.gd
@@ -2,5 +2,7 @@ extends PanelContainer
 
 onready var conversation_output_label = get_node("HBoxContainer/VBoxContainer/ConversationOutput")
 
+var tourist
+
 func set_conversation_output(text):
 	conversation_output_label.bbcode_text = text

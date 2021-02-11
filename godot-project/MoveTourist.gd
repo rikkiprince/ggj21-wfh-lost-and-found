a3faa0bbd43da3bbc334ccb4c38b21c3da4e43e3
Rikki Prince <rfp@ecs.soton.ac.uk>
2021-02-11 00:16:44 -0700

    Jazz up movement handling

--- godot-project/MoveTourist.gd
+++ godot-project/MoveTourist.gd
@@ -22,10 +22,18 @@ export(Array,String) var list_of_directions #["south","left","straight","right",
 
 var destination = Vector2(18,11)
 
+var compass_directions_regex
+var relative_directions_regex
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position_on_map = tilemap.position_on_map(self)
 	direction = STOOD_STILL
+	
+	compass_directions_regex = RegEx.new()
+	compass_directions_regex.compile("north|east|south|west")
+	relative_directions_regex = RegEx.new()
+	relative_directions_regex.compile("left|right|straight|up|down")
 
 
 func pick_random_direction():
@@ -57,23 +65,31 @@ func screen_relative_to_compass(current_direction, relative_direction):
 
 func convert_direction_text_to_vector(current_direction, direction_text):
 	direction_text = direction_text.to_lower()
-	if(["north","east","south","west"].has(direction_text)):
-		print("Got compass direction: "+direction_text)
-		var new_compass_direction = direction_text[0].to_upper()
+	var compass_match = compass_directions_regex.search(direction_text)
+	if(compass_match):
+		var compass_direction = compass_match.get_string()
+		print("Got compass direction: "+compass_direction)
+		var new_compass_direction = compass_direction[0].to_upper()
 		if(new_compass_direction in self):
 			return self.get(new_compass_direction)
 		else:
 			print("ERROR: Could not find vector for compass direction "+str(new_compass_direction))
-	elif(["left","right","straight","up","down"].has(direction_text)):
-		if(direction == STOOD_STILL):
-			return screen_relative_to_compass(current_direction, direction_text)
-		else:
-			return relative_to_compass(current_direction, direction_text)
 	else:
-		print("WARNING: Unknown direction "+str(direction_text))
+		var relative_match = relative_directions_regex.search(direction_text)
+		if(relative_match):
+			var relative_direction = relative_match.get_string()
+			if(direction == STOOD_STILL):
+				return screen_relative_to_compass(current_direction, relative_direction)
+			else:
+				return relative_to_compass(current_direction, relative_direction)
+		else:
+			print("WARNING: Unknown direction "+str(direction_text))
+	return STOOD_STILL # TODO: Make this "straight"?
 
 func get_next_direction(current_direction):
 	var direction_text = list_of_directions.pop_front()
+	print("\n---\n")
+	print("= Processing: '"+direction_text+"'")
 	if(direction_text == null or direction_text == ""):
 		return STOOD_STILL # pick_random_direction() ?
 	
@@ -89,7 +105,7 @@ func next_cell_valid(new_direction):
 	var current_cell = tilemap.position_on_map(self)
 	var new_cell = current_cell + new_direction
 	var name_of_next_cell = tilemap.name_of_tile(new_cell)
-	return ["HorizontalRoad", "VerticalRoad"].has(name_of_next_cell)
+	return ["HorizontalRoad", "VerticalRoad", "Intersection"].has(name_of_next_cell)
 
 func kickstart():
 	direction = get_next_direction(direction)
